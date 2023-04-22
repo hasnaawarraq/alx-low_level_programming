f376465a0d37fe72e31f1e4a2fc292c240d7525e
hasnaawarraq <waraqhasnaa5@example.com>
2023-04-21 19:57:44 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Smile in the mirror
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
+
