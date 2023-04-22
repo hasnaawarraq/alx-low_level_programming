875265e966923881ebbbfd56de40edf7f31d054c
hasnaawarraq <waraqhasnaa5@example.com>
2023-04-21 19:54:10 -0700

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ * main -Numberz
  *
  * Return: Always 0 (Success)
  */
